398a624a894d5be7330507406395e881a861ef86
SayeedMahdi <sayeedmahdimousavi789@gmail.com>
2023-05-02 23:45:53 +0430

    add all the balls and number

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,49 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() {
-  runApp(
-    const MaterialApp(
-      home: null,
-    ),
-  );
+// Creates a Material App
+void main() => runApp(
+      MaterialApp(
+        home: BallPage(),
+      ),
+    );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[100],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[600],
+        title: Text('Wellcome ask me anything.'),
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+// State full for numbering balls
+class Ball extends StatefulWidget {
+  Ball({Key key}) : super(key: key);
+
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: () {
+          setState(() {
+            ballNumber = Random().nextInt(5) + 1;
+          });
+        },
+        child: Image.asset('images/ball$ballNumber.png'),
+      ),
+    );
+  }
 }
